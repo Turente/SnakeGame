153f5d44fccf77a9900522d45e90a0da5d178101
Turente <digitproo@mail.ru>
2025-05-22 17:17:49 +0300

    Add count

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import config
 
 
 pygame.init()
+font = pygame.font.SysFont('Arial', 25)
 screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
 pygame.display.set_caption("Змейка")
 
@@ -16,6 +17,12 @@ clock = pygame.time.Clock()
 # Создаём объекты
 snake = Snake(config.BLOCK_SIZE)
 food = Food(config.BLOCK_SIZE, config.WIDTH, config.HEIGHT)
+score = 0
+
+# Функция для отображения счёта
+def draw_score(score):
+    value = font.render(f"Счёт: {score}", True, config.WHITE)
+    screen.blit(value, [10, 10])
 
 # Основной цикл игры
 running = True
@@ -38,9 +45,11 @@ while running:
     # Проверяем, съела ли змейка еду
     if snake.eat_food(food.position):
         food.position = food.random_position()
+        score += 1
 
     # Рисуем всё
     screen.fill(config.BLACK)
+    draw_score(score)
     snake.draw(screen, config.GREEN)
     food.draw(screen, config.WHITE)
 
